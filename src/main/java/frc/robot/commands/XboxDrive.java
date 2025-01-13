83c546228f6e43d47271521a865908b93be2fd06
Nightowl5275 <139814989+Nightowl5275@users.noreply.github.com>
2025-01-13 18:47:11 -0500

    made XboxController command

---
+++ src/main/java/frc/robot/commands/XboxDrive.java
@@ -0,0 +1,45 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommaandXboxController;
+import frc.robot.subsystems.Drivetrain;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class XboxDrive extends Command {
+  private Drivetrain driveTrain;
+  private CommandXboxController xboxController;
+  /** Creates a new XboxDrive. */
+  public XboxDrive(Drivetrain driveTrain, CommandXboxController xboxController) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    this.driveTrain = driveTrain;
+    this.xboxController = xboxController;
+
+    addRequirements(driveTrain);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    driveTrain.arcadeDrive(xboxController.getLeftX(),xboxController.getRightY());
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    driveTrain.arcadeDrive(0,0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,34 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.pheonix6.hardware.TalonFX;
+
+import com.ctre.pheonix6.CANBus;
+
+public class Drivetrain extends SubsystemBase {
+  /** Creates a new Drivetrain. */
+  private final CANBus kCANBus = new CANBus("canivore");
+  private final TalonFX m_leftLeader = new TalonFX(1, kCANBus);
+  private final TalonFX m_rightFollower = new TalonFX(2, kCANBus);
+  private final TalonFX m_rightLeader = new TalonFX(3, kCANBus);
+  private final TalonFX m_leftFollower = new TalonFX(4, kCANBus);
+
+  public Drivetrain() {
+    m_leftFollower.setControl(new Follower(m_leftLeader.getDeviceID(), false));
+    m_rightFollower.setControl(new Follower(m_rightLeader.getDeviceID(), false));
+  }
+
+  public void arcadeDrive(double speedLeft, double speedRight){
+      m_leftLeader.set(speedLeft);
+      m_rightLeader.set(speedRight);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
