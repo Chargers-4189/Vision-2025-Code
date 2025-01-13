5aa6b9f551d988affbe22f1ee91426cc9fc9b9a5
almort08 <143033800+almort08@users.noreply.github.com>
2025-01-13 18:43:07 -0500

    add drivetrain and drivetrain command
    remove example files and autos

---
+++ src/main/java/frc/robot/subsystems/studentDriveTrain.java
@@ -0,0 +1,37 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.CANBus;
+import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class studentDriveTrain extends SubsystemBase {
+
+  private final CANBus kCANBus = new CANBus("carnavor");
+
+  private final TalonFX leftLeader = new TalonFX(1, kCANBus);
+  private final TalonFX leftFollower = new TalonFX(2, kCANBus);
+  private final TalonFX rightLeader = new TalonFX(3, kCANBus);
+  private final TalonFX rightFollower = new TalonFX(4, kCANBus);
+  /** Creates a new studentDriveTrain. */
+  public studentDriveTrain() {
+    leftFollower.setControl(new Follower(leftLeader.getDeviceID(), false));
+    rightFollower.setControl(new Follower(rightLeader.getDeviceID(), false));
+  }
+
+  public void arcadeDrive(double leftSpeed , double rightSpeed){
+    leftLeader.set(leftSpeed);
+    rightLeader.set(rightSpeed);
+  } 
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
