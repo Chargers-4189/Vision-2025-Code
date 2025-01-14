51eb4341a8ae2809fa74694afb5c81a150afbd1a
Jack Koster <110984012+jlkoster08@users.noreply.github.com>
2025-01-14 18:20:16 -0500

    Update XboxDrive.java

--- src/main/java/frc/robot/commands/XboxDrive.java
+++ src/main/java/frc/robot/commands/XboxDrive.java
@@ -32,7 +32,7 @@ public class XboxDrive extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    driveTrain.tankDrive(xboxController.getLeftX(), xboxController.getLeftY());
+    driveTrain.tankDrive(xboxController.getLeftX(), xboxController.getRightX());
   }
 
   // Called once the command ends or is interrupted.
