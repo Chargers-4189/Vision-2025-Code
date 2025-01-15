c7a5ef8e8a81c3dae16bc4c344970b50ca6628c2
Nightowl5275 <139814989+Nightowl5275@users.noreply.github.com>
2025-01-14 19:11:28 -0500

    Fix bug (getleftY)

--- src/main/java/frc/robot/commands/XboxDrive.java
+++ src/main/java/frc/robot/commands/XboxDrive.java
@@ -28,7 +28,7 @@ public class XboxDrive extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    driveTrain.arcadeDrive(xboxController.getLeftX(),xboxController.getRightY());
+    driveTrain.arcadeDrive(xboxController.getLeftY(),xboxController.getRightY());
   }
 
   // Called once the command ends or is interrupted.
--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -5,13 +5,13 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import com.ctre.pheonix6.hardware.TalonFX;
+import com.ctre.phoenix6.hardware.TalonFX;
 
-import com.ctre.pheonix6.CANBus;
+import com.ctre.phoenix6.CANBus;
+import com.ctre.phoenix6.controls.Follower;
 
 public class Drivetrain extends SubsystemBase {
   /** Creates a new Drivetrain. */
-  private final CANBus kCANBus = new CANBus("canivore");
   private final TalonFX m_leftLeader = new TalonFX(1);
   private final TalonFX m_rightFollower = new TalonFX(2);
   private final TalonFX m_rightLeader = new TalonFX(3);
