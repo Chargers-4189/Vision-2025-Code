380d464b45516273c04e2b8095c01f870b9ca4c3
JDTheBomb <jdthebomb06@gmail.com>
2025-01-25 15:48:32 -0500

    changed pose estimator strategy in constructor

--- src/main/java/frc/robot/subsystems/AprilTagCamera.java
+++ src/main/java/frc/robot/subsystems/AprilTagCamera.java
@@ -13,7 +13,12 @@ import org.photonvision.targeting.PhotonTrackedTarget;
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.StructArrayPublisher;
+import edu.wpi.first.networktables.StructArraySubscriber;
+import edu.wpi.first.networktables.StructPublisher;
 
 public class AprilTagCamera {
 
@@ -24,26 +29,23 @@ public class AprilTagCamera {
 
   protected PhotonCamera camera;
   private PhotonPoseEstimator poseEstimator;
+  private NetworkTableInstance networkTableInstance = NetworkTableInstance.getDefault();
+  private StructPublisher<Pose3d> photonRobotPosition = networkTableInstance.getStructTopic("PhotonRobotPosition", Pose3d.struct).publish();
 
   public AprilTagCamera(String cameraName, Transform3d cameraTranslation) {
     camera = new PhotonCamera(cameraName);
     try {
-      tagLayout =
-        AprilTagFieldLayout.loadFromResource(
-          AprilTagFields.k2025Reefscape.m_resourceFile
-        );
+      tagLayout = AprilTagFieldLayout.loadFromResource(
+          AprilTagFields.k2025Reefscape.m_resourceFile);
     } catch (Exception e) {
       System.err.println(e);
     }
-    poseEstimator =
-      new PhotonPoseEstimator(
+    poseEstimator = new PhotonPoseEstimator(
         tagLayout,
-        PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR,
-        cameraTranslation
-      );
+        PoseStrategy.CLOSEST_TO_CAMERA_HEIGHT,
+        cameraTranslation);
     poseEstimator.setMultiTagFallbackStrategy(
-      PhotonPoseEstimator.PoseStrategy.LOWEST_AMBIGUITY
-    );
+        PhotonPoseEstimator.PoseStrategy.LOWEST_AMBIGUITY);
   }
 
   public Pose2d getEstimatedRobotPose() {
@@ -58,51 +60,56 @@ public class AprilTagCamera {
   public void update() {
     var results = camera.getAllUnreadResults();
     var estimatedResult = targetFilter(results);
-    ///ystem.out.println("EstimatedResult"+estimatedResult);
+    /// ystem.out.println("EstimatedResult"+estimatedResult);
     if (estimatedResult != null) {
-			estimateAvailable = true;
+      estimateAvailable = true;
       estimatedPose = estimatedResult.estimatedPose.toPose2d();
     }
   }
-  public void getAprilTagX(){
+
+  public void getAprilTagX() {
     var result = camera.getLatestResult();
     boolean hasTargets = result.hasTargets();
-    if(hasTargets){
+    if (hasTargets) {
       List<PhotonTrackedTarget> targets = result.getTargets();
       PhotonTrackedTarget target = result.getBestTarget();
-      //System.out.println(target.yaw);
+      var estimatedResult = poseEstimator.update(result);
+      System.out.println("Estimated Result:" + estimatedResult.get().estimatedPose);
+      photonRobotPosition.set(estimatedResult.get().estimatedPose);
+      // System.out.println(target.yaw);
     }
-    //result.getBestTarget();
-    //var estimatedresult = poseEstimator.update(result);
-    //PhotonUtils.estimateFieldToRobotAprilTag(null, null, null)
-    //PhotonTrackedTarget testerer = result.targets.get(0);
-    //Pose2d robotPose = PhotonUtils.estimateFieldToRobotAprilTag(testerer.getBestCameraToTarget(),tagLayout.getTagPose(testerer.getFiducialId()),);
-    
+    // result.getBestTarget();
+
+    // PhotonUtils.estimateFieldToRobotAprilTag(null, null, null)
+    // PhotonTrackedTarget testerer = result.targets.get(0);
+    // Pose2d robotPose =
+    // PhotonUtils.estimateFieldToRobotAprilTag(testerer.getBestCameraToTarget(),tagLayout.getTagPose(testerer.getFiducialId()),);
+
   }
 
   private EstimatedRobotPose targetFilter(List<PhotonPipelineResult> results) {
     if (!results.isEmpty()) {
       var result = results.get(results.size() - 1);
-      //System.out.println("Result: "+ result);
+      
+      // System.out.println("Result: "+ result);
       result.targets.removeIf(tag -> {
         double maxDistance = 6.0;
         Transform3d transform = tag.getBestCameraToTarget();
-        return (
-          transform.getX() > maxDistance || transform.getY() > maxDistance
-        );
+        return (transform.getX() > maxDistance || transform.getY() > maxDistance);
       });
-      if (
-        result.hasTargets() &&
-        result.getTargets().size() < 16 &&
-        result.getTargets().size() > 0
-      ) {
-        //for (var i : result.getTargets()) {
-          //System.out.println(i.getFiducialId());
-        //}
-        //System.out.println("Result: "+ result);
+      if (result.hasTargets() &&
+          result.getTargets().size() < 16 &&
+          result.getTargets().size() > 0) {
+        // for (var i : result.getTargets()) {
+        // System.out.println(i.getFiducialId());
+        // }
+        // System.out.println("Result: "+ result);
+        // var estimatedResult = poseEstimator.update(result);
+        // System.out.println("Estimated Result:" + estimatedResult);
+        // System.out.println(estimatedResult);
         var estimatedResult = poseEstimator.update(result);
-        System.out.println("Estimated Result:" + estimatedResult);
-        //System.out.println(estimatedResult);
+      System.out.println("Estimated Result:" + estimatedResult.get().estimatedPose);
+      photonRobotPosition.set(estimatedResult.get().estimatedPose);
         if (estimatedResult.isPresent()) {
           return estimatedResult.get();
         }
